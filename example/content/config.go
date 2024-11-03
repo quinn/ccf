e42416315a958295ffcdb451180aa7a2bf902d43
quinn <q.shanahan@gmail.com>
2024-11-03 17:36:24 -0500

    ai refactor loading

--- example/content/config.go
+++ example/content/config.go
@@ -1,5 +1,8 @@
 package content
 
+// ContentDir is the root directory for content files
+const ContentDir = "content"
+
 // type Post will load a folder called posts
 type Post struct {
 	// Frontmatter
--- example/internal/web/web.go
+++ example/internal/web/web.go
@@ -8,14 +8,21 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.quinn.io/go-astro/example/content"
 	"go.quinn.io/go-astro/example/router"
 	"go.quinn.io/go-astro/internal/assets"
+	collections "go.quinn.io/go-astro/internal/content"
 )
 
 //go:embed public
 var assetsFS embed.FS
 
 func Run() {
+	// Load content before starting server
+	if err := collections.LoadItems[content.Post](content.ContentDir); err != nil {
+		log.Fatalf("failed to load content: %v", err)
+	}
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 
