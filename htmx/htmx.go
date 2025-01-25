58a1ab2a1bb09ef5535d4ff0f2f60423759e0594
quinn <q.shanahan@gmail.com>
2025-01-25 10:07:47 -0500

    htmx updates

--- htmx/htmx.go
+++ htmx/htmx.go
@@ -6,9 +6,17 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func Is(c echo.Context) bool {
+	return c.Request().Header.Get("HX-Request") == "true"
+}
+
 func Redirect(c echo.Context, path string) error {
-	c.Response().Header().Set("HX-Redirect", path)
-	return c.NoContent(http.StatusOK)
+	if Is(c) {
+		c.Response().Header().Set("HX-Redirect", path)
+		return c.NoContent(http.StatusOK)
+	}
+
+	return c.Redirect(http.StatusFound, path)
 }
 
 func Refresh(c echo.Context) error {
